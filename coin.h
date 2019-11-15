6b9a47f77ce126383d48236e90ede52f7b6681ea
lyricidal <lyricidal@gmail.com>
2019-11-14 21:19:46 -0500

    Update coin.h for DAPS

--- coin.h
+++ coin.h
@@ -7,24 +7,19 @@
 
 #include <string>
 
-static const std::string mainnet_seeds[] = {"seed.bitcoin.sipa.be", // Pieter Wuille, only supports x1, x5, x9, and xd
-                                            "dnsseed.bluematt.me", // Matt Corallo, only supports x9
-                                            "dnsseed.bitcoin.dashjr.org", // Luke Dashjr
-                                            "seed.bitcoinstats.com", // Christian Decker, supports x1 - xf
-                                            "seed.bitcoin.jonasschnelli.ch", // Jonas Schnelli, only supports x1, x5, x9, and xd
-                                            "seed.btc.petertodd.org", // Peter Todd, only supports x1, x5, x9, and xd
+static const std::string mainnet_seeds[] = {"seed.dapscoin-seeds.com",
+                                            "seed2.dapscoin-seeds.com",
+                                            "seed3.dapscoin-seeds.com",
+                                            "seed4.dapscoin-seeds.com",
+                                            "seed5.dapscoin-seeds.com",
                                             ""};
 
-static const std::string testnet_seeds[] = {"testnet-seed.bitcoin.jonasschnelli.ch",
-                                            "seed.tbtc.petertodd.org",
-                                            "seed.testnet.bitcoin.sprovoost.nl",
-                                            "testnet-seed.bluematt.me",
-                                            ""};
+static const std::string testnet_seeds[] = {""};
 
-static const int mainnet_port = 8333;
-static const int testnet_port = 18333;
+static const int mainnet_port = 53572;
+static const int testnet_port = 53574;
 
-static unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
-static unsigned char pchMessageStart_testnet[4] = { 0x0b, 0x11, 0x09, 0x07 };
+static unsigned char pchMessageStart[4] = { 0xa4, 0xb7, 0x79, 0x84 };
+static unsigned char pchMessageStart_testnet[4] = { 0xa4, 0xb7, 0x79, 0x84 };
 
 #endif // __INCLUDED_COIN_H__
