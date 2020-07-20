ee73aa86fe4c5bd1689a932356fe2da594f48bda
lyricidal <lyricidal@gmail.com>
2020-07-20 09:24:27 -0400

    Update blocks to 535000

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 320000;
+    return testnet ? 0 : 535000;
 }
 
 std::string static inline ToString(const CService &ip) {
